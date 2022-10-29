a85d82f2e94a2d4fafe04e4745f3a3dc16ef84b9
Md. Muntasir Enam <muntasir.coder@gmail.com>
2022-10-29 14:20:30 +0600

    farewell

--- src/main/java/tech/muntasir/githubpkg/Greetings.java
+++ src/main/java/tech/muntasir/githubpkg/Greetings.java
@@ -3,6 +3,10 @@ package tech.muntasir.githubpkg;
 public class Greetings {
 
     public static String greet(final String name) {
-        return "Hello dear " + name;
+        return "Hello dear " + name + "!";
+    }
+
+    public static String farewell(final String name) {
+        return "Farewell dear " + name + ". See you soon!";
     }
 }
